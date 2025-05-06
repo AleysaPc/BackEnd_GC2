768eaaabc5fca567219e853c8e1b8d03ed9efb80
unknown <cruzmaralis172813@email.com>
2025-05-06 00:15:51 -0400

    Datos_contacto para concatenación

--- contacto/serializers.py
+++ contacto/serializers.py
@@ -7,9 +7,13 @@ class InstitucionSerializer(serializers.ModelSerializer):
         model = Institucion
         fields = '__all__'
 class ContactoSerializer(serializers.ModelSerializer):  
-    #Esto sale en el frontend.
+    institucion = InstitucionSerializer()
+    nombre_completo = serializers.SerializerMethodField()
+    #Esto sale en el frontend
     class Meta:
         model = Contacto
         fields = '__all__'
 
-  
+  #Esto sale en el frontend.
+    def get_nombre_completo(self, obj):
+        return f"{obj.nombre_contacto} {obj.apellido_pat_contacto} {obj.apellido_mat_contacto} - {obj.titulo_profesional} - {obj.institucion.razon_social}"
--- correspondencia/serializers.py
+++ correspondencia/serializers.py
@@ -2,9 +2,10 @@ from .models import Correspondencia, DocEntrante, DocSaliente, DocInterno
 from rest_framework import serializers
 from documento.serializers import DocumentoSerializer
 from contacto.serializers import ContactoSerializer 
+from documento.models import Documento
 
 class CorrespondenciaListSerializer(serializers.ModelSerializer):
-    documentos = DocumentoSerializer(many=True, read_only=True, required=False)
+    documentos = DocumentoSerializer(many=True)
     contacto = serializers.StringRelatedField()
     
     class Meta: 
@@ -25,6 +26,7 @@ class CorrespondenciaListSerializer(serializers.ModelSerializer):
                 #'usuario', #Usuario que crea la correspondencia
                 
             ]
+
 class CorrespondenciaDetailSerializer(serializers.ModelSerializer):
     documentos = DocumentoSerializer(many=True, read_only=True, required=False)
     contacto = serializers.StringRelatedField()
@@ -50,14 +52,15 @@ class CorrespondenciaDetailSerializer(serializers.ModelSerializer):
 
 class DocEntranteSerializer(serializers.ModelSerializer):
     
-    documentos = DocumentoSerializer(many=True, read_only=True, required=False) 
-    contacto = serializers.StringRelatedField()
+    documentos = DocumentoSerializer(many=True) 
+    datos_contacto = serializers.StringRelatedField(source='contacto', read_only=True)
     class Meta:
         model = DocEntrante
-        fields = '__all__'   
+        fields = '__all__'               
 
 class DocSalienteSerializer(serializers.ModelSerializer):
-    contacto = serializers.StringRelatedField()
+
+    datos_contacto = serializers.StringRelatedField(source='contacto', read_only=True)
 
     class Meta:
         model = DocSaliente
