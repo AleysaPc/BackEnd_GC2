95b4fe9022982bc28d01b6da752637e8547f5536
unknown <cruzmaralis172813@email.com>
2026-03-22 00:53:49 -0400

    MEDIA_ROOT para archivos

--- gestion_documental/settings.py
+++ gestion_documental/settings.py
@@ -133,6 +133,9 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 MEDIA_URL = "/media/"
 #MEDIA_ROOT = BASE_DIR / "media" #Para desarrollo local
 MEDIA_ROOT = "/app/media"
+# Crear carpeta si no existe (Railway)
+import os
+os.makedirs(MEDIA_ROOT, exist_ok=True)
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
